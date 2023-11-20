ceebeb38f73df2ddf36becce255f2f8dbbe6f55a
superfilar <mateusz9650@gmail.com>
2023-11-20 20:53:21 +0100

    hw08 v1.0

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const itemsArray =[];
 
-console.log(galleryItems);
+galleryItems.forEach(element => {
+	const galleryLink = document.createElement('a');
+	galleryLink.className = 'gallery__link';
+	galleryLink.href = element.original;
+
+	const galleryImage = document.createElement('img');
+	galleryImage.className = 'gallery__image';
+	galleryImage.src = element.preview;
+	galleryImage.setAttribute('title', element.description);
+	galleryImage.alt = element.description;
+
+	galleryLink.append(galleryImage);
+	itemsArray.push(galleryLink);
+})
+gallery.append(...itemsArray);
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
