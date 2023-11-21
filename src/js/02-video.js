037acc4ddd9491fabc70f08ab8f82c20e39dfe24
superfilar <mateusz9650@gmail.com>
2023-11-21 19:42:39 +0100

    hw08 v1.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,13 @@
-import Player from '@vimeo/player';
-import { throttle } from 'lodash.throttle';
+import Player from '/node_modules/@vimeo/player';
+import throttle from '/node_modules/lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-player.on('timeupdate',  throttle( e => {
+player.on('timeupdate',  throttle( (e) => {
     localStorage.setItem('videoplayer-current-time', e.seconds);
     }, 1000)
     );
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
-    console.error(error)
+    console.error(error);
 });
